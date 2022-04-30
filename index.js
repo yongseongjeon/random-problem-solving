a831e801a43bad6de3e0b3209482258343e38b87
BB-choi <choibobae0809@gmail.com>
2022-05-01 00:51:37 +0900

    feat: 현재 뽑은 문제 로컬스토리지에 저장 기능 구현

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const problems = [...Object.values(workbookProblems)].join().split(",");
 
 const MIN_PROBLEM_IDX = 0;
 const MAX_PROBLEM_IDX = problems.length - 1;
+const LOCALSTORAGE_KEY_NAME = "random-PS";
 
-const { getRandNum } = util;
+const { getRandNum, localStorage } = util;
 
 const isEmpty = (value) => {
   return !value;
@@ -40,6 +41,48 @@ const getRandomProblems = (count) => {
   return results;
 };
 
+const saveSelectedProblems = (problemIds) => {
+  if (!confirm("현재 선택된 문제들을 제외 목록에 추가하시겠습니까?")) {
+    return;
+  }
+
+  let existProblems = [];
+  if (localStorage.getLocalStorage(LOCALSTORAGE_KEY_NAME)) {
+    existProblems = [...localStorage.getLocalStorage(LOCALSTORAGE_KEY_NAME)];
+  }
+
+  const curTargetProblems = [...existProblems, ...problemIds];
+
+  localStorage.setLocalStorage(LOCALSTORAGE_KEY_NAME, curTargetProblems);
+  console.log;
+
+  if (
+    JSON.stringify([...localStorage.getLocalStorage(LOCALSTORAGE_KEY_NAME)]) !==
+    JSON.stringify([...curTargetProblems])
+  ) {
+    alert("저장에 문제가 발생했습니다. 다시 시도해주세요.");
+    return;
+  }
+
+  alert("저장되었습니다. 앞으로 이 문제들을 제외하고 문제를 뽑습니다.");
+};
+
+const createSaveButtonArea = (problemIds) => {
+  const saveButtonArea = document.createElement("div");
+  const infoMsg = `<p class="info">아래 버튼을 누르시면 앞으로는 위 문제들을 제외하고 문제들을 뽑습니다.</p>`;
+  const saveButton = document.createElement("button");
+
+  saveButton.innerText = "현재 선택된 문제들 제외하기";
+  saveButton.addEventListener("click", () => {
+    saveSelectedProblems(problemIds);
+  });
+
+  saveButtonArea.innerHTML = infoMsg;
+  saveButtonArea.appendChild(saveButton);
+
+  return saveButtonArea;
+};
+
 const showResult = (results) => {
   resultArea.innerHTML = "";
 
@@ -48,7 +91,9 @@ const showResult = (results) => {
   resultUl.innerHTML = results
     .map((result) => `<li>${problems[result]}</li>`)
     .join("");
+
   resultArea.appendChild(resultUl);
+  resultArea.appendChild(createSaveButtonArea(results));
 };
 
 const init = () => {
--- util/util.js
+++ util/util.js
@@ -2,6 +2,20 @@ const util = {
   getRandNum: (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
+  localStorage: {
+    setLocalStorage: (key, value) => {
+      return localStorage.setItem(key, JSON.stringify(value));
+    },
+    getLocalStorage: (key) => {
+      if (!localStorage.getItem(key)) {
+        return;
+      }
+      return JSON.parse(localStorage.getItem(key));
+    },
+    removeFromLocalStorage: (key) => {
+      localStorage.removeItem(key);
+    },
+  },
 };
 
 export default util;
